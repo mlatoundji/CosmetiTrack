c48834169fffd304b369b07df1ef48275fe6b1be
MMyster <mouradlatoundji@gmail.com>
2025-03-28 13:38:21 +0100

    Add .env.example file and update .gitignore for production environment
    - Created a new .env.example file to provide a template for environment variables, including database connection and NextAuth configuration.
    - Updated .gitignore to exclude .env.production to prevent sensitive information from being tracked.
    - Adjusted vercel.json to remove sensitive database URL and NextAuth secret, enhancing security.

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9e138b4a996ac942")
+/******/ 		__webpack_require__.h = () => ("4b1eff63a79fbbc0")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0e5ba5795238e032"; }
+/******/ 		__webpack_require__.h = function() { return "38cac51ed9933d3e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
